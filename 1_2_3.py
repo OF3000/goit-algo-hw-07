8d20e7b575ce32a110a9eca9196bc200de41c729
OF3000 <ofpost@i.ua>
2024-05-25 17:58:02 +0300

    init

---
+++ 1_2_3.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+## Завдання 1
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+## Завдання 2
+def max_value_node(node):
+    current = node
+    while current.right:
+        current = current.right
+    return current
+
+## Завдання 3
+def sum_value_node(node):
+    if node.right and node.left:
+        sum = node.val + sum_value_node(node.right) + sum_value_node(node.left)
+    elif node.right:
+        sum = node.val + sum_value_node(node.right)
+    elif node.left:
+        sum = node.val + sum_value_node(node.left)
+    else: 
+        sum = node.val
+    return sum
+
+if __name__ == "__main__":
+    root = Node(5)
+    root = insert(root, 3)
+    root = insert(root, 2)
+    root = insert(root, 4)
+    root = insert(root, 7)
+    root = insert(root, 6)
+    root = insert(root, 8)
+
+    print(root)
+
+## Завдання 1
+    print(f"Min = {min_value_node(root)}")
+## Завдання 2
+    print(f"Max = {max_value_node(root)}")
+## Завдання 3
+    print(f"Sum = {sum_value_node(root)}")
